a0436d543b523cb2bb1c62a351628a1433d51ff8
sophiasabelja <54071882+sophiasabelja@users.noreply.github.com>
2019-10-13 08:59:41 -0400

    Create indexscripts.js

---
+++ indexscripts.js
@@ -0,0 +1,14 @@
+// if (sessionStorage.getItem('userKey') != null &&
+//     sessionStorage.getItem('userKey') != "null") {
+//     window.location.href = "host.html";
+// }
+
+/* <input id="med"></input>
+<button id="subBut"></button>
+let med = document.querySelector('#med');
+let submit = document.querySelector('#subBut');
+submit.addEventListener('click', onClick)
+function onCLick() {
+    database.push(med.value);
+}
+*/
